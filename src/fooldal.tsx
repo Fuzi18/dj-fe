bacd67f8c74b3453b2cc207497796885fe67d097
Benio-sama <75315916+Benio-sama@users.noreply.github.com>
2023-03-28 09:01:51 +0200

    commit

--- src/fooldal.tsx
+++ src/fooldal.tsx
@@ -1,9 +1,16 @@
 import { Component, ReactNode } from "react";
+import kep from './dj.jpg';
 
 
 export default class Főoldal extends Component {
     render(): ReactNode {
-        return
+        return<div>
+            <p>Üdvözöllek DJ Petrik weboldalán!</p>
+
+            <p>Válassz a fenti lehetőségek közül!</p>
+
+            <img src={kep} alt="DJ" className="kep"/>
+        </div>
     }
 
 }
--- src/szakmaitapasztalat.tsx
+++ src/szakmaitapasztalat.tsx
@@ -1,8 +1,27 @@
 import { Component, ReactNode } from "react";
+import profile from './petrik.jpg';
 
 
 export default class Szakmaitapasztalat extends Component {
     render(): ReactNode {
-        return
+        return<div>
+
+            <img src={profile} alt="Petrik" className="petrik"/>
+
+            <p>Pro-bono bulik</p>
+            <ul>
+                        <li>Csibeavató, 2021. 08. 31.</li>
+                        <li>Karácsonyi bál, 2021. 12. 22.</li>
+                        <li>Farsangi mulatság, 2022. 02. 20.</li>
+                    </ul>
+
+            <p>Professzionális bulik</p>
+            <ul>
+                        <li>Tanárkarácsony, 2021. 12. 20.</li>
+                        <li>Ballagás, 2022. 04. 30.</li>
+                    </ul>
+                       
+        </div>
+
     }
 }

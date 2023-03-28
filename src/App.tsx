3aaf67484e26e4045a073a117340e8cdc8075f15
Benio-sama <75315916+Benio-sama@users.noreply.github.com>
2023-03-28 08:39:00 +0200

    start

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Szakmaitapasztalat from './szakmaitapasztalat';
+import Idpontfoglalas from './idopontfoglalas';
+import { Route, Routes } from 'react-router-dom';
+import Főoldal from './fooldal';
+import Nav from './nav';
+import Footer from './footer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <><Nav/><Routes>
+        <Route path="/" element={<Főoldal />}></Route>
+        <Route path="/tapasztalat" element={<Szakmaitapasztalat />}></Route>
+        <Route path="/idopont" element={<Idpontfoglalas />}></Route>
+      </Routes><Footer/></>
   );
 }
-
 export default App;

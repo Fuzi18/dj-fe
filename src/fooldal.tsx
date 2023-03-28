3a048ba8fb60414bd81d65db690f1aa9ff817712
Benio-sama <75315916+Benio-sama@users.noreply.github.com>
2023-03-28 09:20:27 +0200

    s

--- src/fooldal.tsx
+++ src/fooldal.tsx
@@ -5,9 +5,9 @@ import kep from './dj.jpg';
 export default class Főoldal extends Component {
     render(): ReactNode {
         return<div>
-            <p>Üdvözöllek DJ Petrik weboldalán!</p>
+            <p className="foszoveg">Üdvözöllek DJ Petrik weboldalán!</p>
 
-            <p>Válassz a fenti lehetőségek közül!</p>
+            <p className="foszoveg">Válassz a fenti lehetőségek közül!</p>
 
             <img src={kep} alt="DJ" className="kep"/>
         </div>
--- src/idopontfoglalas.tsx
+++ src/idopontfoglalas.tsx
@@ -2,7 +2,21 @@ import { Component, ReactNode } from "react";
 
 
 export default class Idpontfoglalas extends Component {
+    
+    submit = async () =>{
+        return window.alert("Köszönjük a megkeresését");
+    }
     render(): ReactNode {
-        return
+        return<div className="kapcsolat">
+            <form onSubmit={this.submit}>
+                <input type="text" required  placeholder="Név" className="xd"/>
+                <br/>
+                <input type="date" className="xd"/>
+                <br/>
+                <textarea  required placeholder="Leírás" className="xd"/>
+                <br/>
+                <button className="xd">Küldés</button>
+            </form>
+        </div>
     }
 }
